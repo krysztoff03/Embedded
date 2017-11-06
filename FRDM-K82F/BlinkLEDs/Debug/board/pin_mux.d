5ed76e0d8ebd2be62758462cddc82b237a72e95d
krysztoff03 <cristi.iosif03@gmail.com>
2017-11-06 18:59:39 +0200

    Add files via upload

---
+++ FRDM-K82F/BlinkLEDs/Debug/board/pin_mux.d
@@ -0,0 +1,34 @@
+board/pin_mux.o: ../board/pin_mux.c ../CMSIS/fsl_device_registers.h \
+ ../CMSIS/MK82F25615.h ../CMSIS/core_cm4.h ../CMSIS/core_cmInstr.h \
+ ../CMSIS/cmsis_gcc.h ../CMSIS/core_cmFunc.h ../CMSIS/core_cmSimd.h \
+ ../CMSIS/system_MK82F25615.h ../CMSIS/MK82F25615_features.h \
+ ../drivers/fsl_port.h ../drivers/fsl_common.h ../drivers/fsl_clock.h \
+ ../board/pin_mux.h ../drivers/fsl_gpio.h
+
+../CMSIS/fsl_device_registers.h:
+
+../CMSIS/MK82F25615.h:
+
+../CMSIS/core_cm4.h:
+
+../CMSIS/core_cmInstr.h:
+
+../CMSIS/cmsis_gcc.h:
+
+../CMSIS/core_cmFunc.h:
+
+../CMSIS/core_cmSimd.h:
+
+../CMSIS/system_MK82F25615.h:
+
+../CMSIS/MK82F25615_features.h:
+
+../drivers/fsl_port.h:
+
+../drivers/fsl_common.h:
+
+../drivers/fsl_clock.h:
+
+../board/pin_mux.h:
+
+../drivers/fsl_gpio.h:
---
+++ FRDM-K82F/BlinkLEDs/Debug/source/main.d
@@ -0,0 +1,42 @@
+source/main.o: ../source/main.c ../board/board.h \
+ ../utilities/fsl_debug_console.h ../drivers/fsl_common.h \
+ ../CMSIS/fsl_device_registers.h ../CMSIS/MK82F25615.h \
+ ../CMSIS/core_cm4.h ../CMSIS/core_cmInstr.h ../CMSIS/cmsis_gcc.h \
+ ../CMSIS/core_cmFunc.h ../CMSIS/core_cmSimd.h \
+ ../CMSIS/system_MK82F25615.h ../CMSIS/MK82F25615_features.h \
+ ../drivers/fsl_clock.h ../drivers/fsl_common.h ../board/pin_mux.h \
+ ../board/clock_config.h ../drivers/fsl_gpio.h
+
+../board/board.h:
+
+../utilities/fsl_debug_console.h:
+
+../drivers/fsl_common.h:
+
+../CMSIS/fsl_device_registers.h:
+
+../CMSIS/MK82F25615.h:
+
+../CMSIS/core_cm4.h:
+
+../CMSIS/core_cmInstr.h:
+
+../CMSIS/cmsis_gcc.h:
+
+../CMSIS/core_cmFunc.h:
+
+../CMSIS/core_cmSimd.h:
+
+../CMSIS/system_MK82F25615.h:
+
+../CMSIS/MK82F25615_features.h:
+
+../drivers/fsl_clock.h:
+
+../drivers/fsl_common.h:
+
+../board/pin_mux.h:
+
+../board/clock_config.h:
+
+../drivers/fsl_gpio.h:
